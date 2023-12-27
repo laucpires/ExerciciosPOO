9b3a50b7fb09fc209dcc78c62d8141a16e460a24
Laura Cargnelutti Pires <laucpires5@gmail.com>
2023-12-27 17:18:05 -0300

    Create index.php

---
+++ 03/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>03</title>
+</head>
+<body>
+    <form action="./03.php" method="get">
+        <label for="largura">Largura:</label>
+        <input type="number" name="largura">
+        <br>
+        <label for="altura">Altura:</label>
+        <input type="number" name="altura">
+        <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
