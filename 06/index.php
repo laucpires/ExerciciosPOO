3721a7fbaf60964da68b64d4e1d3aab7ee3cb4c8
Laura Cargnelutti Pires <laucpires5@gmail.com>
2023-12-27 17:21:47 -0300

    Create index.php

---
+++ 06/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>06</title>
+</head>
+<body>
+    <form action="./06.php" method="get">
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome">
+        <br>
+        <label for="preco">Preço:</label>
+        <input type="number" name="preco" step="0.01">
+        <br>
+        <label for="qtdEstoque">Quantidade em Estoque:</label>
+        <input type="number" name="qtdEstoque">
+        <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
