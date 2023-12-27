5644d431f2730085f2893a58300aa975431cb43d
Laura Cargnelutti Pires <laucpires5@gmail.com>
2023-12-27 17:13:40 -0300

    Create index.php

---
+++ 01/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>01</title>
+</head>
+
+<body>
+    <form action="./01.php" method="get">
+        <label for="name">Raio:</label>
+        <input type="text" name="raio">
+        <input type="submit">
+    </form>
+</body>
+
+</html>
