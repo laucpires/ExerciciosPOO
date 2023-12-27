30ac14929b10a8bfe2b75b0e83a743ccb09a4e78
Laura Cargnelutti Pires <laucpires5@gmail.com>
2023-12-27 17:20:35 -0300

    Create index.php

---
+++ 05/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>05</title>
+</head>
+<body>
+    <form action="./05.php" method="get">
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome">
+        <br>
+        <label for="salario">Salário:</label>
+        <input type="number" name="salario" step="0.01">
+        <br>
+        <label for="cargo">Cargo:</label>
+        <input type="text" name="cargo">
+        <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
