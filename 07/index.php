27913ddf75c5551c8b62adba3494cac3f22a9a0a
Laura Cargnelutti Pires <laucpires5@gmail.com>
2023-12-27 17:22:47 -0300

    Create index.php

---
+++ 07/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>07</title>
+</head>
+<body>
+    <form action="./07.php" method="get">
+        <h3>Defina os lados do triângulo:</h3>
+        <label for="lado1">Lado 1:</label>
+        <input type="number" name="lado1" step="0.01">
+        <br>
+        <label for="lado1">Lado 2:</label>
+        <input type="number" name="lado2" step="0.01">
+        <br>
+        <label for="lado1">Lado 3:</label>
+        <input type="number" name="lado3" step="0.01">
+        <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
