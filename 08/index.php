fb22946278c9b77dff2904d42c4b22ae2357c51d
Laura Cargnelutti Pires <laucpires5@gmail.com>
2023-12-28 15:38:12 -0300

    Create index.php

---
+++ 08/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>08</title>
+</head>
+<body>
+    <form action="./08.php" method="get">
+        <label for="marca">Marca:</label>
+        <input type="text" name="marca">
+        <br>
+        <label for="modelo">Modelo:</label>
+        <input type="text" name="modelo">
+        <br>
+        <label for="velocidade">Velocidade:</label>
+        <input type="number" name="velocidade">
+        <br>
+        <label for="aceleracao">Aceleração:</label>
+        <input type="number" name="aceleracao">
+        <br>
+        <label for="diminuicao">Diminuição:</label>
+        <input type="number" name="diminuicao">
+        <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
