71e224efbbf2253ce89be3219637fa70c93d1de8
Laura Cargnelutti Pires <laucpires5@gmail.com>
2023-12-28 17:22:40 -0300

    Create index.php

---
+++ 09/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>09</title>
+</head>
+<body>
+    <form action="./09.php" method="get">
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome">
+        <br>
+        <label for="idade">Idade:</label>
+        <input type="number" name="idade">
+        <br>
+        <label for="consulta">Consulta:</label>
+        <input type="text" name="consulta">
+        <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
